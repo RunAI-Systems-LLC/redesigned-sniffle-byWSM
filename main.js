b94252eca0b1e775197e8520c038be58e6f1b5a4
idWXM <210317822+idWXM@users.noreply.github.com>
2025-08-04 13:25:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+function googleLogin() { alert("Login com Google em breve."); }
+function microsoftLogin() { alert("Login com Microsoft em breve."); }
+function appleLogin() { alert("Login com Apple em breve."); }
+function emailLogin() { alert("Login com Email em breve."); }
